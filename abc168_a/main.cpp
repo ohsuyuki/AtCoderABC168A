a69ef8b4d55f156f592cc2f86881bc92e58a4692
ohsuyuki <papu8osu@gmail.com>
2020-05-25 01:42:55 +0900

    nitial commit

--- abc168_a/main.cpp
+++ abc168_a/main.cpp
@@ -9,7 +9,30 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N = 0;
+    std::cin >> N;
+
+    int div = N % 10;
+    std::string unit = "";
+    switch (div) {
+        case 2:
+        case 4:
+        case 5:
+        case 7:
+        case 9:
+            unit = "hon";
+            break;
+        case 0:
+        case 1:
+        case 6:
+        case 8:
+            unit = "pon";
+            break;
+        default:
+            unit = "bon";
+    }
+
+    std::cout << unit;
+
     return 0;
 }
